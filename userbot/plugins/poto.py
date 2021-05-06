ccf34afaf7e9f3957ccc9b1c23414445193a69d7
jasem-iq <83676607+jasem-iq@users.noreply.github.com>
2021-05-06 12:02:29 +0300

    Update poto.py

--- userbot/plugins/poto.py
+++ userbot/plugins/poto.py
@@ -1,24 +1,10 @@
-#    Friendly Telegram (telegram userbot)
-#    Copyright (C) 2018-2019 The Authors
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 ----------------------------------------------------------------
 All Thenks goes to Emily ( The creater of This Plugin)
-\nSome credits goes to me ( @kirito6969 ) for ported this plugin
-\nand `SnapDragon for` Helping me.
+\nSome credits goes to me ( @RRRD7 ) for ported this plugin
+\nand `klanr for` Helping me @Jmthon .
 ----------------------------------------------------------------
 
 Type `.poto` for get **All profile pics of that User**
@@ -45,7 +31,7 @@ async def potocmd(event):
         uid = 1
         if int(uid) > (len(photos)):
             return await edit_delete(
-                event, "`No photo found of this NIBBA / NIBBI. Now u Die!`"
+                event, "عـذرا الشـخص لايـضع صور "
             )
         send_photos = await event.client.download_media(photos[uid - 1])
         await event.client.send_file(event.chat_id, send_photos)
@@ -60,21 +46,21 @@ async def potocmd(event):
                     photo = await event.client.download_profile_photo(event.input_chat)
                 await event.client.send_file(event.chat_id, photo)
             except Exception:
-                return await edit_delete(event, "`This user has no photos to show you`")
+                return await edit_delete(event, "هذا المستخدم ليس لديه صور لتظهر لك ")
     else:
         try:
             uid = int(uid)
             if uid <= 0:
                 await edit_or_reply(
-                    event, "```number Invalid!``` **Are you Comedy Me ?**"
+                    event, "الـرقم غـير صحـيح "
                 )
                 return
         except BaseException:
-            await edit_or_reply(event, "`Are you comedy me ?`")
+            await edit_or_reply(event, "خـطأ ")
             return
         if int(uid) > (len(photos)):
             return await edit_delere(
-                event, "`No photo found of this NIBBA / NIBBI. Now u Die!`"
+                event, "هـذا المستـخدم ليـس لديـة صـور لتـضهر لـك "
             )
 
         send_photos = await event.client.download_media(photos[uid - 1])
