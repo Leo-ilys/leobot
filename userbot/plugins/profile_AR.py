7cd28ea91822677b8a82bf4730f530cf8b038341
jasem-iq <83676607+jasem-iq@users.noreply.github.com>
2021-05-07 20:00:51 +0300

    Update profile_AR.py

--- userbot/plugins/profile_AR.py
+++ userbot/plugins/profile_AR.py
@@ -155,9 +155,9 @@ async def count(event):
     await event.edit(result)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern=r"حذف"))
+@bot.on(admin_cmd(outgoing=True, pattern=r"مسح"))
 async def remove_profilepic(delpfp):
-    """امر حذف الصور - لحذ صوره واحد من حسابك او جميعها"""
+    """امر حذف الصور - لحذف صوره واحد من حسابك او جميعها"""
     group = delpfp.text[8:]
     if group == "الصور":
         lim = 0
